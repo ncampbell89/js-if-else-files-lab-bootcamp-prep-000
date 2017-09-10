358311f38dd69205cb0c22755187f0d8143af465
Nicole Campbell <nscampbell303@gmail.com>
2017-09-10 01:20:57 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -6,14 +6,6 @@ if (index.name === "Susan") {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
-if (index.height === "70") {
-    console.log("The height is correct")
-} else {
-  console.log("Expected: 70, Received: "+index.height)
-}
-
-
-
 if (index.height === 70) {
     console.log("The height is correct")
 } else {
